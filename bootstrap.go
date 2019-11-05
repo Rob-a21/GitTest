9321fc1570a3b6db66dd2903520eb07fa852bba6
Robel <56542460+Rob-a21@users.noreply.github.com>
2019-11-06 01:57:40 +0300

    second

--- bootstrap.go
+++ bootstrap.go
@@ -5,37 +5,46 @@ import (
 	"net/http"
 )
 
-type first struct {
-	Name string
-	ID   string
+func main() {
+	//New Multiplexer
+	mux := http.NewServeMux()
+	//file server
+	fs := http.FileServer(http.Dir("assets"))
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	//handler
+	mux.HandleFunc("/", index)
+	mux.HandleFunc("/other", other)
+	mux.HandleFunc("/struct", struc)
+
+	//Starts the server
+	http.ListenAndServe(":8080", mux)
 }
 
+//Template 1
 var templ = template.Must(template.ParseFiles("index.html"))
-var tem = template.Must(template.ParseFiles("other.html"))
-var t = template.Must(template.ParseFiles("struc.html"))
-
-func structServe(w http.ResponseWriter, r *http.Request) {
-	user := first{"Robel", "ATR/4874/10"}
-	t.Execute(w, user)
-}
 
 func index(w http.ResponseWriter, r *http.Request) {
 	templ.Execute(w, nil)
 }
 
+//Template 2
+var templ2 = template.Must(template.ParseFiles("other.html"))
+
 func other(w http.ResponseWriter, r *http.Request) {
-	tem.Execute(w, nil)
+	templ2.Execute(w, nil)
 }
 
-func main() {
-	mux := http.NewServeMux()
-	//File Server
-	fs := http.FileServer(http.Dir("assets"))
-	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
-	//Handler
-	mux.HandleFunc("/index", index)
-	mux.HandleFunc("/other", other)
-	mux.HandleFunc("/struct", structServe)
-	//Listener
-	http.ListenAndServe(":8000", mux)
+//Struct
+type person struct {
+	Name string
+	ID   string
+}
+
+//Template 3
+var templ3 = template.Must(template.ParseFiles("struc.html"))
+
+func struc(w http.ResponseWriter, r *http.Request) {
+	person := person{"Robel", "ATR/4874/10"}
+	templ3.Execute(w, person)
 }
